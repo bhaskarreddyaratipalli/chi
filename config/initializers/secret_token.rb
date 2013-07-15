e7a86420df193dba8192a9413791fad61bb3547d
bhaskarreddy <bhaskarreddy.aratipalli@gmail.com>
2013-07-15 11:02:07 +0530

    hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bapp::Application.config.secret_token = 'f5c9d607b1955f04179a4162240f3b28e5cb2f75ef833179a0afc55172aa2afe10566e6fb6c16e72cc83ada04657e5c8f283504d386b9b8b438921d9e2f0c607'
